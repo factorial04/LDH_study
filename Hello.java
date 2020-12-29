b4c07ae600f50935ae9ea4fbb6bdd0602220cddf
factorial <69578143+factorial04@users.noreply.github.com>
2020-12-30 02:06:46 +0900

    Hello.java

---
+++ Hello.java
@@ -0,0 +1,10 @@
+clss Whilesum{
+  public static void main(String args[]){
+    int i = 1, sum = 0;
+    while(i<100){
+      sum = sum + 1;
+      i++;
+     }
+     System.out.println("1에서 99까지의 합은" +sum);
+    }
+  }
